f407116f1ad1733ad7138573f0c92443b31bbcd6
Lilidales <phamtrieu02@gmail.com>
2023-12-19 19:56:12 +1100

    Update Flask to get gdp data

--- app_solution.py
+++ app_solution.py
@@ -12,20 +12,9 @@ from flask import render_template
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("postgresql://bichjennings:ciL8Vd5SjUMY@ep-white-night-07545349.ap-southeast-1.aws.neon.tech/P3G2?sslmode=require")
 
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(autoload_with=engine)
-conn = engine.connect()
-#data = conn.execute(text("SELECT * FROM world_population limit 10"))
-df = pd.read_sql("SELECT * FROM world_population",conn)
-print(df.head(2).to_json())
-#results = list(data)
-#print(results[0])
-# Save reference to the table
-# Passenger = Base.classes.passenger
+
+
 
 # #################################################
 # # Flask Setup
@@ -49,21 +38,26 @@ def welcome1():
     """List all available api routes."""
     return results
 
-# @app.route("/api/v1.0/names")
-# def names():
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
+@app.route("/api/gdp")
+def gdp_data():
+    # reflect an existing database into a new model
+    engine = create_engine(
+        "postgresql://bichjennings:ciL8Vd5SjUMY@ep-white-night-07545349.ap-southeast-1.aws.neon.tech/P3G2?sslmode=require")
 
-#     """Return a list of all passenger names"""
-#     # Query all passengers
-#     results = session.query(Passenger.name).all()
+    Base = automap_base()
+    # reflect the tables
+    Base.prepare(autoload_with=engine)
+    session = Session(engine)
+    conn = engine.connect()
+    table_name="gpd_data"
+    query = f"SELECT * FROM {table_name};"
+    results = session.execute(text(query))
+    results_data=[
+        {'country_name':result.country_name,'country_code':result.country_code,
+         'year':result.year,'gdp':result.gdp} for result in results
+    ]
 
-#     session.close()
-
-#     # Convert list of tuples into normal list
-#     all_names = list(np.ravel(results))
-
-#     return jsonify(all_names)
+    return jsonify({table_name+'_data': results_data}), 200
 
 
 # @app.route("/api/v1.0/passengers")
