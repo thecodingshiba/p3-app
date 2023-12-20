b094183d8e543962bf0efec9f5aa7bccc6a71840
Lilidales <phamtrieu02@gmail.com>
2023-12-20 16:25:38 +1100

    created dropdown option

---
+++ static/Duc_plot.js
@@ -0,0 +1,41 @@
+// Define the URL for the JSON file
+const jsonUrl = "http://127.0.0.1:5000/api/population";
+
+//Functions
+function optionChanged(selectedValue) {
+    // Perform actions based on the selected value
+    console.log("Selected country:", selectedValue);
+  }
+
+  function initiateDropdown(){
+        // Container and dropdown selection
+        const container = d3.select("#well");
+        const dropdown = d3.select("#selDataset");
+    
+        // Add an initial loading option
+        dropdown.append("option")
+        .attr("value", "loading")
+        .text("Loading...");
+
+        d3.json(jsonUrl)
+.then(x=>{
+    // Container and dropdown selection
+    const container = d3.select("#well");
+    const dropdown = d3.select("#selDataset");
+
+    // Remove the loading option
+    dropdown.select("option[value='loading']").remove();
+
+    // Create options for the dropdown
+    const options = dropdown
+    .selectAll("option")
+    .data(x.country.sort())
+    .enter()
+    .append("option")
+    .attr("value", d => d)
+    .text(d => d);
+    // Assign the value of the dropdown menu option to a variable
+    let dataset = dropdown.property("value");
+})
+  }
+initiateDropdown()
